8c51e622f3afd58a0683e22a92962cb8b4eaea94
Kanzu <kanzu32@gmail.com>
2021-08-18 20:55:35 +0300

    Update comicsMerge.py
    rar support, fixes and improvements

--- comicsMerge.py
+++ comicsMerge.py
@@ -12,33 +12,58 @@ from progress.bar import Bar
 
 init(convert=True)
 
-endTypes = [".zip", ".cbz"]
+zipEndTypes = [".zip", ".cbz"]
+rarEndTypes = [".rar", ".cbr"]
 
 errorsCount = 0
 errors = []
 
 while True:
-    bookPartsDir = input("Enter folder name with files to merge: ")
-    bookPath = os.path.join(root, bookPartsDir)
+    bookPartsDir = input("Enter folder name with files to merge (in input folder): ")
+    bookPath = os.path.join(root, "input", bookPartsDir)
     if (bookPartsDir == ""):
         print(colored("folder name is empty", "red"))
-    elif (os.path.exists(bookPath) and not bookPartsDir == ""):
-        break
-    else:
+    elif (os.path.exists(bookPath) and len(os.listdir(bookPath)) == 0):
+        print(colored("folder is empty", "red"))
+    elif (not os.path.exists(bookPath)):
         print(colored("folder \"{0}\" does not exist".format(bookPartsDir), "red"))
+    else:
+        break
+
+overwrite = 0
 
 while True:
-    endName = input("Enter merged file name with file type (.cbz or .zip, <name of the input folder>.cbz by default): ") or "{0}.cbz".format(bookPartsDir)
+    endName = input("Enter merged file name with file type (.cbz, .zip, .rar, .cbr support. <name of the input folder>.cbz by default): ") or "{0}.cbz".format(bookPartsDir)
+    if (endName in os.listdir(os.path.join(root, "output"))):
+        print(colored("Name \"{0}\" already existed.".format(endName), "red"))
+        while True:
+            overwriteInput = input("Overwrite it? y/n: ")
+            if overwriteInput.upper() == "Y":
+                overwrite = 1
+                break
+            elif overwriteInput.upper() == "N":
+                overwrite = 0
+                break
+            else:
+                print(colored("Please use Y or N.", "red"))
+        if (not overwrite):
+            continue
     endType = endName[endName.rfind("."):]
-    if endType in endTypes:
+    if (endType in zipEndTypes):
+        endTypeMode = "zip"
+        break
+    elif (endType in rarEndTypes):
+        endTypeMode = "rar"
         break
     else:
-        print(colored("unsupported file type \"{0}\"".format(endType), "red"))
+        print(colored("Unsupported file type \"{0}\"".format(endType), "red"))
 
 if ("output" not in os.listdir()):
     os.mkdir("output")
-
-endFile = zipfile.ZipFile(os.path.join("output", endName), "w")
+if ("temp" not in os.listdir()):
+    os.mkdir("temp")
+if ("input" not in os.listdir()):
+    os.mkdir("input")
 
 bookPartsList = os.listdir(bookPath)
 
@@ -46,48 +71,106 @@ progressBar = Bar('Processing', max=len(bookPartsList))
 progressBar.update()
 
 filesCount = 1
-for part in bookPartsList:
-    partPath = os.path.join(bookPath, part)
+if (endTypeMode == "zip"):
+    endFile = zipfile.ZipFile(os.path.join("output", endName), "w", allowZip64 = True)
 
-    if os.path.isdir(partPath):
-        filesList = os.listdir(partPath)
-        for i in range(len(filesList)):
-            item = open(os.path.join(partPath, filesList[i]), "rb")
-            itemData = item.read()
-            fileType = filesList[i][filesList[i].rfind("."):]
-            endFile.writestr(str(filesCount) + fileType, itemData)
-            filesCount += 1
-        progressBar.next()
+    for part in bookPartsList:
+        partPath = os.path.join(bookPath, part)
+        if os.path.isdir(partPath):
+            filesList = os.listdir(partPath)
+            for i in range(len(filesList)):
+                item = open(os.path.join(partPath, filesList[i]), "rb")
+                itemData = item.read()
+                fileType = filesList[i][filesList[i].rfind("."):]
+                endFile.writestr(str(filesCount) + fileType, itemData)
+                item.close()
+                filesCount += 1
+            progressBar.next()
 
-    elif zipfile.is_zipfile(partPath):
-        arhive = zipfile.ZipFile(partPath, "r")
-        filesList = arhive.namelist()
-        for i in range(len(filesList)):
-            fileType = filesList[i][filesList[i].rfind("."):]
-            endFile.writestr(str(filesCount) + fileType, arhive.read(filesList[i]))
-            filesCount += 1
-        arhive.close()
-        progressBar.next()
+        elif zipfile.is_zipfile(partPath):
+            arhive = zipfile.ZipFile(partPath, "r")
+            filesList = arhive.namelist()
+            for i in range(len(filesList)):
+                fileType = filesList[i][filesList[i].rfind("."):]
+                itemData = arhive.read(filesList[i])
+                endFile.writestr(str(filesCount) + fileType, itemData)
+                filesCount += 1
+            arhive.close()
+            progressBar.next()
 
-    elif rarfile.is_rarfile(partPath):
-        arhive = rarfile.RarFile(partPath, "r")
-        filesList = arhive.namelist()
-        for i in range(len(filesList)):
-            fileType = filesList[i][filesList[i].rfind("."):]
-            endFile.writestr(str(filesCount) + fileType, arhive.read(filesList[i]))
-            filesCount += 1
-        arhive.close()
-        progressBar.next()
+        elif rarfile.is_rarfile(partPath):
+            arhive = rarfile.RarFile(partPath, "r")
+            filesList = arhive.namelist()
+            for i in range(len(filesList)):
+                fileType = filesList[i][filesList[i].rfind("."):]
+                itemData = arhive.read(filesList[i])
+                endFile.writestr(str(filesCount) + fileType, itemData)
+                filesCount += 1
+            arhive.close()
+            progressBar.next()
 
-    else:
-        errorsCount += 1
-        errors.append(part)
+        else:
+            errorsCount += 1
+            errors.append(part)
+    endFile.close()
 
+elif (endTypeMode == "rar"):
+    endFile = os.path.join(root, "output", endName + endType)
+    for part in bookPartsList:
+        partPath = os.path.join(bookPath, part)
+        if os.path.isdir(partPath):
+            filesList = os.listdir(partPath)
+            for i in range(len(filesList)):#copy with change names
+                fileType = filesList[i][filesList[i].rfind("."):]
+                item = open(os.path.join(partPath, filesList[i]), "rb")
+                itemData = item.read()
+                tempFile = open(os.path.join("temp", "{0}".format(filesCount) + fileType), "wb")
+                tempFile.write(itemData)
+                tempFile.close()
+                item.close()
+                filesCount += 1
+            progressBar.next()
 
-endFile.close()
+        elif zipfile.is_zipfile(partPath):
+            arhive = zipfile.ZipFile(partPath, "r")
+            filesList = arhive.namelist()
+            for i in range(len(filesList)):
+                fileType = filesList[i][filesList[i].rfind("."):]
+                itemData = arhive.read(filesList[i])
+                tempFile = open(os.path.join("temp", "{0}".format(filesCount) + fileType), "wb")
+                tempFile.write(itemData)
+                tempFile.close()
+                filesCount += 1
+            arhive.close()
+            progressBar.next()
+
+        elif rarfile.is_rarfile(partPath):
+            arhive = rarfile.RarFile(partPath, "r")
+            filesList = arhive.namelist()
+            for i in range(len(filesList)):
+                fileType = filesList[i][filesList[i].rfind("."):]
+                itemData = arhive.read(filesList[i])
+                tempFile = open(os.path.join("temp", "{0}".format(filesCount) + fileType), "wb")
+                tempFile.write(itemData)
+                tempFile.close()
+                filesCount += 1
+            arhive.close()
+            progressBar.next()
+
+        else:
+            errorsCount += 1
+            errors.append(part)
+
+    arhivePath = os.path.join("output", endName)
+    filePath = "temp"
+    if overwrite:
+        os.system("@echo off & rar d {0}>Nul".format(arhivePath))
+    
+    os.system("@echo off & rar m -ep {0} {1}>Nul".format(arhivePath, filePath)) #-ep1
+
 progressBar.finish()
 if (errorsCount > 0):
     print(colored("Unsupported files: {0}".format(errorsCount), "yellow"))
     print(colored("\n".join(errors), "yellow"))
-print(Fore.GREEN + "Done " + Style.RESET_ALL)
+print(colored("Done", "green"))
 time.sleep(3)
